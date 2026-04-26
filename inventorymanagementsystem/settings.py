9856774bbfc553b13da278aaa3f9fbcb0e5fe5ef
Mohan Chaudhary <chymohan12@gmail.com>
2026-04-26 11:13:04 +0545

    Configure authentication, media URLs, and DRF settings

--- inventorymanagementsystem/settings.py
+++ inventorymanagementsystem/settings.py
@@ -121,11 +121,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 AUTH_USER_MODEL = 'core.User'
+LOGIN_URL = '/login/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
